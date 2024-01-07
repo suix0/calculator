2aed1665639d1b6799207fe17264bdb85e5bd206
suix0 <112095627+suix0@users.noreply.github.com>
2024-01-07 20:07:11 +0800

    Added operations function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Create functions for each operations
 function add(...nums) {
   return nums.reduce((sum, num) => sum + num)
 }
@@ -14,4 +15,20 @@ function division(...nums) {
   return nums.reduce((divisionResult, num) => divisionResult / num);
 }
 
+// Create a function to do some operations
+function operate(operation, ...nums) {
+  let result = 0;
+
+  if (operation === '+') {
+    result = add(...nums);
+  } else if (operation === '-') {
+    result = subtract(...nums);
+  } else if (operation === 'x') {
+    result = multiply(...nums);
+  } else if (operation === '÷') {
+    result = division(...nums);
+  }
+
+  return result;
+}
 
